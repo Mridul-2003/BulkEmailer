f8671bb846cbf7415e0df6a62fd863261774bd11
Mridul-2003 <110475111+Mridul-2003@users.noreply.github.com>
2024-12-23 16:51:02 +0530

    Update mailer.py

--- mailer.py
+++ mailer.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com' 
 app.config['MAIL_PORT'] = 587                
-app.config['MAIL_USERNAME'] = 'gauranshi@genaisummit.in'  
-app.config['MAIL_PASSWORD'] = 'gttl bzjs btqn ddkl'          
+app.config['MAIL_USERNAME'] = 'Anant@genaisummit.in'  
+app.config['MAIL_PASSWORD'] = 'fxgi hoqz umlg ikej'           
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 
